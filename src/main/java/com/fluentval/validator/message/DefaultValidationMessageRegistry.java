37a43e3a57b84ee371afe69a6a5d36e964b58301
Matej Saric <matejsaric8@outlook.com>
2025-05-14 17:42:29 +0200

    release(fixing DefaultValidationMessageRegistry provider not implementing ValidationMessageRegistry)

--- src/main/java/com/fluentval/validator/message/DefaultValidationMessageRegistry.java
+++ src/main/java/com/fluentval/validator/message/DefaultValidationMessageRegistry.java
@@ -6,7 +6,7 @@ import com.fluentval.validator.metadata.DefaultValidationCode;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DefaultValidationMessageRegistry {
+public class DefaultValidationMessageRegistry implements ValidationMessageRegistry {
     private final Map<String, ValidationMessageProvider> providers = new HashMap<>();
     private ValidationMessageProvider defaultProvider;
 
@@ -20,6 +20,7 @@ public class DefaultValidationMessageRegistry {
         registerProvider(defaultProvider);
     }
 
+    @Override
     public void registerProvider(ValidationMessageProvider provider) {
         for (DefaultValidationCode code : DefaultValidationCode.values()) {
             if (provider.supports(code.getCode())) {
@@ -28,15 +29,18 @@ public class DefaultValidationMessageRegistry {
         }
     }
 
+    @Override
     public void setProviderForCode(String code, ValidationMessageProvider provider) {
         providers.put(code, provider);
     }
 
+    @Override
     public String getMessage(String code, ValidationIdentifier identifier, Map<String, Object> parameters) {
         ValidationMessageProvider provider = providers.getOrDefault(code, defaultProvider);
         return provider.getMessage(code, identifier, parameters);
     }
 
+    @Override
     public void setDefaultProvider(ValidationMessageProvider provider) {
         this.defaultProvider = provider;
         registerProvider(provider);
